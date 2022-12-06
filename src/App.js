c36553667f257ead4c999818fecd576bc0842262
Fran Linde Blazquez <franlindebl@gmail.com>
2022-12-06 12:14:38 +0100

    Added mortgage calculator

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import InputSample from './InputSample/InputSample';
+import MortgageCalcultator from './MortgageCalcultator/MortgageCalcultator';
 
 function App() {
   return (
     <div className="App">
+      {/* calculadora de hipotecas */}
+      <MortgageCalcultator></MortgageCalcultator>
+
+      <hr className="separator"></hr>
+
+      {/* Ejemplo de input simple */}
       <InputSample></InputSample>
     </div>
   );
---
+++ src/MortgageCalcultator/MortgageCalcultator.js
@@ -0,0 +1,81 @@
+import React from "react";
+import "./MortgageCalcultator.css";
+
+const MortgageCalcultator = () => {
+
+    // Estados
+    const [monthlyPayment, setMonthlyPayment] = React.useState(0);
+
+    // Referencias a los inputs
+    const houseValueRef = React.useRef();
+    const savingsRef = React.useRef();
+    const numYearsRef = React.useRef();
+    const annualInterestRef = React.useRef();
+
+    const getValuesAndCalculateMonthlyPayment = () => {
+
+        const houseValue = houseValueRef.current.value;
+        const savings = savingsRef.current.value;
+        const annualInterest = annualInterestRef.current.value;
+        const numYears = numYearsRef.current.value;
+
+        const payment = calculateMonthPayment(houseValue, savings, annualInterest, numYears);
+        setMonthlyPayment(payment);
+    }
+
+    const calculateMonthPayment = (houseValue, savings, annualInterest, numYears) => {
+        const numMonths = numYears * 12;
+        const annualInterestDecimal = annualInterest / 100;
+        const monthlyInterest = annualInterestDecimal / 12;
+        const moneyToAsk = houseValue - savings;
+        const divider = (1 - Math.pow(1 + monthlyInterest, -numMonths)) / monthlyInterest;
+        const monthPayment = moneyToAsk / divider;
+        return monthPayment;
+    }
+
+    return (
+        <div className="mortage-calculator">
+            <h2>Calculadora de hipotecas</h2>
+
+            {/* valor de la casa */}
+            <fieldset className="mortgage-calculator__fieldset">
+                <label>
+                    Introduce el valor de la casa:
+                    <input ref={houseValueRef} placeholder="300000" type="number" name="houseValue" id="houseValue" />
+                </label>
+            </fieldset>
+
+            {/* ahorros aportados */}
+            <fieldset className="mortgage-calculator__fieldset">
+                <label>
+                    Introduce el ahorros aportados:
+                    <input ref={savingsRef} placeholder="30000" type="number" name="savings" id="savings" />
+                </label>
+            </fieldset>
+
+            {/* plazo en años */}
+            <fieldset className="mortgage-calculator__fieldset">
+                <label>
+                    Introduce el plazo en años:
+                    <input ref={numYearsRef} placeholder="30" type="number" name="numYears" id="numYears" />
+                </label>
+            </fieldset>
+
+            {/* interés de la hipoteca (tipo fijo) */}
+            <fieldset className="mortgage-calculator__fieldset">
+                <label>
+                    Introduce el interés de la hipoteca (tipo fijo):
+                    <input ref={annualInterestRef} placeholder="2" type="number" name="annualInterest" id="annualInterest" />
+                </label>
+            </fieldset>
+
+
+            <button onClick={getValuesAndCalculateMonthlyPayment}>Calcular cuota mensual</button>
+
+            <p>Tu cuota mensual será de: <strong>{monthlyPayment}</strong></p>
+        </div>
+    );
+
+}
+
+export default MortgageCalcultator;
